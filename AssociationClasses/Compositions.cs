5a26703532de39a3944f0e83ff0bb3c70a88ea31
June Cho <junecho.ucb@gmail.com>
2017-09-24 11:52:34 -0700

    Added Composition Benefits

--- AssociationClasses/Compositions.cs
+++ AssociationClasses/Compositions.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿////Composition= Relationship between Two Classes that allows one to contain the other
+////*Benefits
+////1) Code Re-USe
+////2) Flexibilty
+////3) A means to Loose-Coupling Apps   
+
+using System;
 namespace CP3.AssociationClasses
 {
     public class Compositions
