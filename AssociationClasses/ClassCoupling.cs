6946f0806c9f98660e1e4ec1ac9210adde7f1e07
June Cho <junecho.ucb@gmail.com>
2017-09-24 09:58:54 -0700

    Loosely Coupled

--- AssociationClasses/ClassCoupling.cs
+++ AssociationClasses/ClassCoupling.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿////Class Coupling = A measure of how interconnected classes and subsystems are
+
+using System;
 namespace CP3.AssociationClasses
 {
     public class ClassCoupling
