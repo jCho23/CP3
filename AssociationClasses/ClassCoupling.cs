fc73bb9705450291416bd0b3d421aff565b1e6a0
June Cho <junecho.ucb@gmail.com>
2017-09-24 10:01:06 -0700

    Encapulation Notes

--- AssociationClasses/ClassCoupling.cs
+++ AssociationClasses/ClassCoupling.cs
@@ -1,5 +1,13 @@
 ﻿////Class Coupling = A measure of how interconnected classes and subsystems are
 
+////*BAD* Tightly Coupled= An app that have Classes tightly related to each other 
+////*GOOD* Loosely Coupled= Not Dependent on other Classes
+
+////How to Design Loosely Coupled?
+////*Encapulation= Using Access Modifiers, restrict what other Classes see
+////*Relationship between Classes
+////*Interfaces   
+
 using System;
 namespace CP3.AssociationClasses
 {
