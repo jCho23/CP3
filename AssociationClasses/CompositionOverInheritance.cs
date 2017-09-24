7b6d5e0106b2e5c5d05e8a330ec8c2625a41db55
June Cho <junecho.ucb@gmail.com>
2017-09-24 12:14:02 -0700

    Problems with Inheritance

--- AssociationClasses/CompositionOverInheritance.cs
+++ AssociationClasses/CompositionOverInheritance.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿////Problems with Inheritance
+//// 1) Easily abused by amateur designers / developers
+//// 2) Large Hierarchies
+//// 3) Fragility
+//// 4) Tightly Coupling 
+
+
+
+using System;
 namespace CP3.AssociationClasses
 {
     public class CompositionOverInheritance
