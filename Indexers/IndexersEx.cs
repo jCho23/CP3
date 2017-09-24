662f7562d8117c9575baaa3d567f1c9bc62fc29e
June Cho <junecho.ucb@gmail.com>
2017-09-23 22:15:41 -0700

    Created Demo1

--- Indexers/IndexersEx.cs
+++ Indexers/IndexersEx.cs
@@ -1,7 +1,15 @@
 ﻿using System;
 
 ////Indexers = A way to access elements in a calss that represents a list of values
- 
+
+//Demo1
+//var array = new int[5];
+////This is an Indexer
+////Noti 
+array[0]= 1;
+
+var list = new List<int>();
+list[0]= 1;
 
 
 namespace CP3.Indexers
