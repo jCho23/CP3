37f007d3d660f0e0d1517f9b97c2cd48a70e219d
June Cho <junecho.ucb@gmail.com>
2017-09-23 22:18:17 -0700

    fin Demo2

--- Indexers/IndexersEx.cs
+++ Indexers/IndexersEx.cs
@@ -2,14 +2,16 @@
 
 ////Indexers = A way to access elements in a calss that represents a list of values
 
-//Demo1
+////Demo1
 //var array = new int[5];
 ////This is an Indexer
 ////Notice that we can call the first number of the Index here 
 //array[0]= 1;
 
-//Demo2
+////Demo2
+////Here, we have a list of int
 var list = new List<int>();
+////Again, using a Indexer, we can access the first element in the list
 list[0]= 1;
 
 
