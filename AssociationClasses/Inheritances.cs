3a045872a65491f0ed08b1978bb262ae6511664b
June Cho <junecho.ucb@gmail.com>
2017-09-24 11:19:32 -0700

    Added Syntax Inheritance Demo

--- AssociationClasses/Inheritances.cs
+++ AssociationClasses/Inheritances.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿////Inheritance = A kind of relationship between two classes that allows one to inherit code from the other
+////*Benefits
+//// 1) Code Re-USe = Encapsulates one classs and use that later for another classs
+//// 2) Polymorphic behavior = 
+
+////Syntax Inheritance Demo
+
+using System;
 namespace CP3.AssociationClasses
 {
     public class Inheritances
