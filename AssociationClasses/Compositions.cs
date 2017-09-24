49bafa19d68b8c9a198a7eca5b98e6325b0abb24
June Cho <junecho.ucb@gmail.com>
2017-09-24 12:05:36 -0700

    fin Installer Class

--- AssociationClasses/Compositions.cs
+++ AssociationClasses/Compositions.cs
@@ -46,7 +46,21 @@ namespace CP3.AssociationClasses
         }
     }
 
-    ////
+    ////Installer
+    public class Installer
+    {
+        private readonly Logger _logger;
+
+        public Installer(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Install()
+        {
+            _logger.Log("We are Installing the Application.");
+        }
+    }
 
 
     public class Compositions
